5ac5fdfa2b431c82d1a17ce73da29a59871bf8e9
Ira Lazarenko <lira_2005@bk.ru>
2025-05-19 22:23:17 +0600

    part of user model and serializers

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from foodgram.constants import (USER_NAMES_MAX_LENGTH,
+                                USER_EMAIL_MAX_LENGTH,
+                                USER_NICKNAME_MAX_LENGTH,
+                                USER_REGEX_CONTROLLER)
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """Класс пользователя."""
+
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]  # for superuser
+    USERNAME_FIELD = "email"                                   # login
+
+    first_name = models.CharField(
+        max_length=USER_NAMES_MAX_LENGTH,
+        verbose_name="Имя")
+    last_name = models.CharField(
+        max_length=USER_NAMES_MAX_LENGTH,
+        verbose_name="Фамилия",
+    )
+    email = models.EmailField(
+        max_length=USER_EMAIL_MAX_LENGTH,
+        unique=True,
+        verbose_name="Электронная почта"
+    )
+    username = models.CharField(
+        max_length=USER_NICKNAME_MAX_LENGTH,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=USER_REGEX_CONTROLLER,
+                message="Некорректное имя пользователя")],
+        verbose_name="Имя пользователя",
+    )
+    avatar = models.ImageField(
+        upload_to="avatars/",
+        null=True,
+        blank=True,
+        verbose_name="Фото профиля"
+    )
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ("username",)
+
+    def __str__(self):
+        return self.username
+
+
+class Subscription(models.Model):
+    """Класс отображения подписки."""
+
+    user: models.ForeignKey = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='subscriptions'
+    )
+    author: models.ForeignKey = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='subscribers'
+    )
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'], name='unique_subscription'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} подписан(-а) на {self.author}'
---
+++ backend/users/serializers.py
@@ -0,0 +1,85 @@
+import base64
+import imghdr
+
+from rest_framework import serializers
+from djoser.serializers import UserSerializer
+from django.core.files.base import ContentFile
+from users.models import CustomUser, Subscription
+
+
+class ReformattingBase64(serializers.ImageField):
+    """Переформатирование фото профиля из base64."""
+
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, img_str = data.split(';base64,')
+            ext = imghdr.what(None, h=base64.b64decode(img_str))
+            content = ContentFile(
+                base64.b64decode(img_str),
+                name=f'temp.{ext}')
+
+        return super().to_internal_value(content)
+
+
+class CustomUserSerializer(UserSerializer):
+    """Сериализатор для пользователя."""
+
+    avatar = ReformattingBase64()
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CustomUser
+        fields = UserSerializer.Meta.fields + ('avatar', 'is_subscribed')
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        return (request
+                and request.user.is_authenticated
+                and obj.subscriptions.filter(user=request.user).exists())
+
+
+class SubscriptionSerializer(CustomUserSerializer):
+    """Сериализатор для подписок."""
+
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.ReadOnlyField(source='recipes.count')
+
+    class Meta:
+        model = CustomUser
+        fields = CustomUserSerializer.Meta.fields + (
+            'recipes', 'recipes_count',
+        )
+
+
+class SubscriptionCreateSerializer(serializers.ModelSerializer):
+    """Сериализатор для оформления подписки."""
+
+    class Meta:
+        model = Subscription
+        fields = ('user', 'author')
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Subscription.objects.all(),
+                fields=('user', 'author'),
+                message='Уже подписан на этого пользователя.'
+            )
+        ]
+
+    def validate(self, data):
+        if data['user'] == data['author']:
+            raise serializers.ValidationError('Нельзя подписаться на себя.')
+        return data
+
+    def to_representation(self, instance):
+        return SubscriptionSerializer(instance.author,
+                                      context=self.context).data
+
+
+class AvatarSerializer(serializers.ModelSerializer):
+    """Сериализатор для фото профиля."""
+
+    avatar = ReformattingBase64()
+
+    class Meta:
+        model = CustomUser
+        fields = ('avatar',)
