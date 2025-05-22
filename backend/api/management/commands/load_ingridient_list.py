0348773f0e5e5ecf700f3114492fa596158e2584
Ira Lazarenko <lira_2005@bk.ru>
2025-05-22 19:26:38 +0600

    docker 2

---
+++ backend/api/management/commands/load_ingridient_list.py
@@ -0,0 +1,22 @@
+import json
+from recipes.models import Ingredient
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Загрузка ингредиентов из ingredients.json'
+
+    def handle(self, *args, **kwargs):
+        with open('pre_data/ingredients.json', encoding='utf-8') as file:
+            data = json.load(file)
+            count = 0
+            for item in data:
+                obj, created = Ingredient.objects.get_or_create(
+                    name=item['name'],
+                    measurement_unit=item['measurement_unit']
+                )
+                if created:
+                    count += 1
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'{count} ingredients successfully loaded.'))
